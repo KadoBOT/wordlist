5d9a5d29fa0f858627bbd63a6dd54f2bac9a39be
Ricardo Ambrogi <ricardo.ambrogi@gmail.com>
2016-04-11 15:17:16 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,49 @@
 import React, { Component } from 'react';
 
 export default class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = { words: null, word: null, match: false}
+  }
+
+  handleSubmit(e){
+    e.preventDefault();
+  }
+
+  readFile = (e) =>{
+    let file = e.target.files[0];
+    if (!file) return;
+    let reader = new FileReader();
+    reader.onload = (e) => {
+      let content = e.target.result.split("\n");
+      this.setState({words: content})
+    }
+    reader.readAsText(file);
+  }
+
+  handleChange = (e) =>{
+    this.setState({word: e.target.value});
+  }
+
   render() {
     return (
-      <h1>Hello, world.</h1>
+      <div>
+        <div>
+          <form onSubmit={this.handleSubmit}>
+            <input type="file" onChange={this.readFile} />
+          </form>
+          {this.state.words ?
+            <div>
+              <h4>Insert a word in the box below to see if it matches the words on the file</h4>
+              <input placeholder="word" value={this.state.word} onChange={this.handleChange}/>
+              <h3>Contents of the file:</h3>
+              {this.state.word ? <h6>{`${this.state.word} matches are below in red`}</h6> : null}
+            </div>
+            : null
+          }
+          <pre id="file-content">{this.state.words ? this.state.words.map(word => <p key={word} className={this.state.match}>{word}</p>) : "Upload a file using the button above"}</pre>
+        </div>
+      </div>
     );
   }
 }
